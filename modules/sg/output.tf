e22851a46fdf6ade18816d69b2fa27585f6e91b8
Tom <tom.harpaz98@gmail.com>
2022-10-27 19:50:33 +0300

    added outputs for subnet ids and change d sg module output names

--- modules/sg/output.tf
+++ modules/sg/output.tf
@@ -1,9 +1,9 @@
-output "sg_web_servers" {
+output "sg_id_web_servers" {
   description = "Web servers Security Group ID"
   value       = aws_security_group.web_servers.id
 }
 
-output "sg_load_balancers" {
+output "sg_id_load_balancers" {
   description = "Load balancers Security Group ID"
   value       = aws_security_group.load_balancers.id
 }
